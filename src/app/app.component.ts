9328c6b2245b123e9a9c30a305e12f371afe9092
Coralesk1 <guscoraleski2006@gmail.com>
2025-05-08 12:32:22 -0300

    hello again

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SceneMenuComponent } from "./scene-menu/scene-menu.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SceneMenuComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/scene-menu/scene-menu.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-scene-menu',
+  imports: [],
+  templateUrl: './scene-menu.component.html',
+  styleUrl: './scene-menu.component.css'
+})
+export class SceneMenuComponent {
+
+}
