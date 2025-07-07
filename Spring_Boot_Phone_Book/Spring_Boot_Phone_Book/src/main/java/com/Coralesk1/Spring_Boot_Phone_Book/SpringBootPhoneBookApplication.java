f338af691c33d5c85ade419a10c241a765159c82
Coralesk1 <guscoraleski2006@gmail.com>
2025-07-07 13:19:04 -0300

    Add pasta do Spring Boot

---
+++ Spring_Boot_Phone_Book/Spring_Boot_Phone_Book/src/main/java/com/Coralesk1/Spring_Boot_Phone_Book/SpringBootPhoneBookApplication.java
@@ -0,0 +1,13 @@
+package com.Coralesk1.Spring_Boot_Phone_Book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPhoneBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootPhoneBookApplication.class, args);
+	}
+
+}
---
+++ Spring_Boot_Phone_Book/Spring_Boot_Phone_Book/src/test/java/com/Coralesk1/Spring_Boot_Phone_Book/SpringBootPhoneBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Coralesk1.Spring_Boot_Phone_Book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootPhoneBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
