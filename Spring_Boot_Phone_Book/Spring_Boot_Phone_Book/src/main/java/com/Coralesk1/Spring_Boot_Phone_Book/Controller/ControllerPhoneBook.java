bf39427ef7356609e40f5c37582d79e27f758376
Coralesk1 <guscoraleski2006@gmail.com>
2025-07-21 14:03:49 -0300

    test

--- Spring_Boot_Phone_Book/Spring_Boot_Phone_Book/src/main/java/com/Coralesk1/Spring_Boot_Phone_Book/Controller/ControllerPhoneBook.java
+++ Spring_Boot_Phone_Book/Spring_Boot_Phone_Book/src/main/java/com/Coralesk1/Spring_Boot_Phone_Book/Controller/ControllerPhoneBook.java
@@ -16,7 +16,7 @@ public class ControllerPhoneBook {
 
     @PostMapping("/create")
     public ModelMenssageOK create(@RequestBody @Valid ModelPhoneBook contact ) { // Aqui o Spring já criou e preencheu o objeto com base no JSON recebido
-        return new ModelMenssageOK("Contact created successfully");
+        return new ModelMenssageOK("Contact createdsdfvdsfgsdgsdfgdfgdg successfully");
     }
 
 }
