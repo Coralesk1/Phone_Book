384e5780cb6b8b75e0ec4b5a5f79594ce21873f2
Coralesk1 <guscoraleski2006@gmail.com>
2025-07-21 20:05:23 -0300

    tirando arquivos desnecessarios

--- Spring_Boot_Phone_Book/Spring_Boot_Phone_Book/src/main/java/com/Coralesk1/Spring_Boot_Phone_Book/Repository/ContactRepository.java
+++
@@ -1,9 +0,0 @@
-package com.Coralesk1.Spring_Boot_Phone_Book.Repository;
-
-import com.Coralesk1.Spring_Boot_Phone_Book.Model.ModelPhoneBook;
-import jakarta.persistence.Id;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ContactRepository extends JpaRepository<ModelPhoneBook, Long> {
-
-}
