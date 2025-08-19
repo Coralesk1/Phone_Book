4d08291c4f4726982e6042dfca9a5fc749c9fd95
Coralesk1 <guscoraleski2006@gmail.com>
2025-08-19 10:17:43 -0300

    fazendo um teste unitario no metodo existById()

--- Spring_Boot_Phone_Book/Spring_Boot_Phone_Book/src/test/java/com/Coralesk1/Spring_Boot_Phone_Book/Service/ServicePhoneBookTest.java
+++ Spring_Boot_Phone_Book/Spring_Boot_Phone_Book/src/test/java/com/Coralesk1/Spring_Boot_Phone_Book/Service/ServicePhoneBookTest.java
@@ -1,56 +1,38 @@
 package com.Coralesk1.Spring_Boot_Phone_Book.Service;
 
 import com.Coralesk1.Spring_Boot_Phone_Book.Repository.RepositoryPhoneBook;
-import com.Coralesk1.Spring_Boot_Phone_Book.TestMock.MockContact;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
-
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.when;
 import static org.junit.jupiter.api.Assertions.*;
+
 // Arquivo que contem os testes .
 
-@TestInstance(TestInstance.Lifecycle.PER_CLASS) //ciclo de vida da instância da classe de teste.
-// PER_METHOD): o JUnit cria uma nova instância da classe de teste para cada métooo de teste.
-@ExtendWith(MockitoExtension.class) // anotação pra consegir usar as anotações do Mockito
+@ExtendWith(MockitoExtension.class)
 class ServicePhoneBookTest {
-
-    MockContact entrada; // variável chamada entrada do tipo MockContact
-
-    @InjectMocks
-    //cria uma instancia da class ServicePhoneBook e injeta nela todos os campos que forem @Mock automaticamente
-    private ServicePhoneBook servicePhoneBook;
 
     @Mock
     private RepositoryPhoneBook repository;
 
-    @BeforeEach
-    void setUp() {
-        entrada = new MockContact();
-        MockitoAnnotations.openMocks(this);
-    }
+    @InjectMocks
+    private ServicePhoneBook servicePhoneBook;
 
     @Test
-    void listContact() {
+    void existById() {
+
+    Long id = 7L;
+
+    when(repository.existsById(id)).thenReturn(true);
+    boolean result = servicePhoneBook.existsById(id);
+
+    assertTrue(result);
+    verify(repository, times(1)).existsById(id);
+
     }
 
-    @Test
-    void createContact() {
-    }
 
-    @Test
-    void getContactById() {
-    }
-
-    @Test
-    void deleteContactById() {
-    }
-
-    @Test
-    void updateContact() {
-    }
 }
